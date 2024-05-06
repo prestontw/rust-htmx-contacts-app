6d47699e9f7c23745581a57e12790ec717a43091
Preston TW <preston_tunnell_wilson@brown.edu>
2024-05-06 15:30:57 -0400

    Move from rust-overlay to rust-toolchain
    since cannot get RUST_SRC_PATH and rust-analyzer and nix
    working together

--- flake.nix
+++ flake.nix
@@ -2,38 +2,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "utils";
-      };
-    };
   };
-  outputs = { self, nixpkgs, utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, utils, ... }:
     utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs { inherit system; };
         rustFlags = (if pkgs.stdenv.isLinux then ''
             -C linker=clang -C link-arg=-fuse-ld=${pkgs.mold}/bin/mold
           '' else ''
             -C linker=clang -C link-arg=-fuse-ld=lld -Z threads=8
           '');
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
       {
         devShell = with pkgs; mkShell {
           buildInputs = [
             just
             cargo-watch
-            rustToolchain
+            rustup
             tailwindcss
           ] ++ lib.optionals stdenv.isDarwin [libiconv llvmPackages.bintools]
           ++ lib.optionals stdenv.isLinux [mold clang];
 
           RUSTFLAGS = "${rustFlags}";
-          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/src";
         };
       });
 }
