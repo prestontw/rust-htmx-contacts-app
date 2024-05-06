0c572c449eb9e28b0f7952e53cec701f3dd63651
Preston TW <preston_tunnell_wilson@brown.edu>
2024-05-06 01:57:24 -0400

    Add infra for working on project
    including flake, rust-toolchain file, trying out fmt file,
    and env file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use axum::response::IntoResponse;
+use axum::response::Redirect;
+use axum_extra::routing::TypedPath;
+use serde::Deserialize;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Deserialize, TypedPath)]
+#[typed_path("/")]
+struct Root;
+async fn root(_: Root) -> impl IntoResponse {
+    Redirect::permanent(&Contacts.to_string())
+}
+
+#[derive(Deserialize, TypedPath)]
+#[typed_path("/contacts")]
+struct Contacts;
+async fn contacts(_: Contacts) -> impl IntoResponse {
+    "Hello world"
 }
